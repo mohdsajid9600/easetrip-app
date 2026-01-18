9269450330a967de65b1a52f601c81e5d1753b89
mohdsajid9600 <mohdsajid9600@gmail.com>
2026-01-18 17:56:41 +0530

    we have added a feature to send an email to every cutomer who has booked a cab

--- src/main/java/com/sajidtech/easytrip/service/BookingService.java
+++ src/main/java/com/sajidtech/easytrip/service/BookingService.java
@@ -14,11 +14,11 @@ import com.sajidtech.easytrip.repository.CabRepository;
 import com.sajidtech.easytrip.repository.CustomerRepository;
 import com.sajidtech.easytrip.repository.DriverRepository;
 import com.sajidtech.easytrip.transformer.BookingTransformer;
-import com.sajidtech.easytrip.transformer.CabTransformer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class BookingService {
@@ -34,6 +34,9 @@ public class BookingService {
 
     @Autowired
     private DriverRepository driverRepository;
+
+    @Autowired
+    JavaMailSender javaMailSender;
 
     public BookingResponse bookCab(BookingRequest bookingRequest, int customerId) {
         Customer customer = customerRepository.findById(customerId).orElseThrow(()->
@@ -53,7 +56,39 @@ public class BookingService {
         Customer savedCustomer = customerRepository.save(customer);
         Driver savedDriver = driverRepository.save(driver);
 
-        return BookingTransformer.bookingToBookingResponse(savedBooking, availableCab,savedDriver,savedCustomer);
 
+        BookingResponse bookingResponse = BookingTransformer.bookingToBookingResponse(savedBooking, availableCab,savedDriver,savedCustomer);
+        sendEmail(bookingResponse);
+        return bookingResponse;
+    }
+
+    private void sendEmail(BookingResponse booking){
+        String formate =
+                "Dear "+booking.getCustomerResponse().getName()+",\n\n" +
+                        "Thank you for choosing EasyTrip!\n\n" +
+                        "Your cab booking has been successfully confirmed. Please find your trip details below:\n\n" +
+                        "========================================\n" +
+                        "Passenger Name : "+booking.getCustomerResponse().getName()+"\n\n" +
+                        "Pickup Point   :"+booking.getPickup()+"\n" +
+                        "Drop Point     : "+booking.getDestination()+"\n\n" +
+                        "Total Fare     : ₹"+booking.getBillAmount()+"\n\n" +
+                        "Cab Model      : "+booking.getCabResponse().getCabModel()+"\n" +
+                        "Cab Number     : "+booking.getCabResponse().getCabNumber()+"\n\n" +
+                        "Driver Name    : "+booking.getCabResponse().getDriverResponse().getName()+"\n" +
+                        "Driver Email   : "+booking.getCabResponse().getDriverResponse().getEmail()+"\n" +
+                        "========================================\n\n" +
+                        "Your cab will reach your pickup location shortly.\n\n" +
+                        "We wish you a safe and comfortable journey with EasyTrip.\n\n" +
+                        "Regards,\n" +
+                        "EasyTrip Support Team\n" +
+                        "Customer Care: +91-90000-00000\n" +
+                        "Email: support@easytrip.com";
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("easetriptrevler@gmail.com");
+        simpleMailMessage.setTo(booking.getCustomerResponse().getEmail());
+        simpleMailMessage.setSubject("EasyTrip Booking Confirmation.");
+        simpleMailMessage.setText(formate);
+        javaMailSender.send(simpleMailMessage);
     }
 }
